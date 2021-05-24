b528b21da8117fa7a3750cf6b3a5ae9e2ef77008
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-05-25 08:43:30 +0900

    Update ViewController.swift

--- subject_9/subject_Re9/ViewController.swift
+++ subject_9/subject_Re9/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet private weak var selectedPrefectureLabel: UILabel!
     
     @IBAction func didTapSelectButton(_ sender: Any) {
+        // prefecturePickerにinstantiateメソッドを使ってModalViewControllerをいれる。
+        //ModaiViewControllerの中のdidSelectPrefectureHandlerというクロージャ(なぜか@escapingをつけると非同期処理らしい)が入るプロバティに具体的なクロージャの内容を渡す（引数）
         let prefecturePicker = ModalViewController.instantiate(
             didSelectPrefecture: { [weak self] name in
                 self?.selectedPrefectureLabel.text = name
